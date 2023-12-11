67e244ce4d59a83af532f1869c36ec5bc46ff368
alicia-basulto <satelliteneptune@gmail.com>
2023-12-11 12:16:02 +0100

    feat(1 puzzle): solution for 1st puzzle 3rd December Adventure of Code

--- main.py
+++ main.py
@@ -45,14 +45,14 @@ def first_challenge(filename):
 
 
 def second_challenge(filename):
-    cubes_limit = {'red':12, 'green':13 , 'blue': 14}
+    cubes_limit = {'red': 12, 'green': 13, 'blue': 14}
     try:
         errors = set()
         with open(filename, 'r') as file:
             content = file.readlines()
         for line in content:
             division_id = line.split(':')
-            id =division_id[0].strip("Game")
+            id = division_id[0].strip("Game")
             line_content = division_id[1].split(';')
             # Utilizar expresiones regulares para encontrar los pares color-valor
             for game in line_content:
@@ -69,15 +69,98 @@ def second_challenge(filename):
 
         numeros = [int(cadena.strip()) for cadena in errors]
 
-        todos_los_numeros = list(range(1, len(content)+1))
+        todos_los_numeros = list(range(1, len(content) + 1))
         numeros_no_aparecen = [numero for numero in todos_los_numeros if numero not in numeros]
 
-        print("Sum total:"+str(sum(numeros_no_aparecen)))
+        print("Sum total:" + str(sum(numeros_no_aparecen)))
+
+    except FileNotFoundError:
+        print("The file does not exist.")
+
+
+def surroundingsDigits(content, line_number, line_position, number):
+    # Loop back from position j to the start of the string
+    # Intermediate lines
+    if line_number != len(content) - 1 and line_number != 0:
+        for pos in range(line_position - 1, (line_position - 1) - len(number), -1):
+            if (re.findall(r'[^a-zA-Z0-9.\n]', content[line_number + 1][pos])
+                    or re.findall(r'[^a-zA-Z0-9.\n]', content[line_number + 1][pos + 1])
+                    or re.findall(r'[^a-zA-Z0-9.\n]', content[line_number + 1][pos - 1])
+                    or re.findall(r'[^a-zA-Z0-9.\n]', content[line_number - 1][pos])
+                    or re.findall(r'[^a-zA-Z0-9.\n]', content[line_number - 1][pos + 1])
+                    or re.findall(r'[^a-zA-Z0-9.\n]', content[line_number - 1][pos - 1])
+                    or re.findall(r'[^a-zA-Z0-9.\n]', content[line_number][pos + 1])
+                    or re.findall(r'[^a-zA-Z0-9.\n]', content[line_number][pos - 1])):
+                return True
+        return False
+    # First line
+    elif line_number == 0:
+        for pos in range(line_position - 1, (line_position - 1) - len(number), -1):
+            if (re.findall(r'[^a-zA-Z0-9.\n]', content[line_number + 1][pos])
+                    or re.findall(r'[^a-zA-Z0-9.\n]', content[line_number + 1][pos + 1])
+                    or re.findall(r'[^a-zA-Z0-9.\n]', content[line_number + 1][pos - 1])
+                    or re.findall(r'[^a-zA-Z0-9.\n]', content[line_number][pos + 1])
+                    or re.findall(r'[^a-zA-Z0-9.\n]', content[line_number][pos - 1])):
+                return True
+        return False
+    # Final line
+    elif line_number == len(content) - 1:
+        for pos in range(line_position - 1, (line_position - 1) - len(number), -1):
+            if (re.findall(r'[^a-zA-Z0-9.\n]', content[line_number - 1][pos])
+                    or re.findall(r'[^a-zA-Z0-9.\n]', content[line_number - 1][pos + 1])
+                    or re.findall(r'[^a-zA-Z0-9.\n]', content[line_number - 1][pos - 1])
+                    or re.findall(r'[^a-zA-Z0-9.\n]', content[line_number][pos + 1])
+                    or re.findall(r'[^a-zA-Z0-9.\n]', content[line_number][pos - 1])):
+                return True
+        return False
+
+
+def third_challenge(filename):
+    cubes_limit = {'red': 12, 'green': 13, 'blue': 14}
+    try:
+        errors = set()
+        with open(filename, 'r') as file:
+            content = file.readlines()
+            numbers_subtracted = []
+            numbers_converted = []
+            previous_char_number = False
+            for i, line in enumerate(content):
+                entire_number = ''
+                for j, character in enumerate(line):
+                    if character.isdigit():
+                        previous_char_number = True
+                        entire_number += character
+                    else:
+                        if previous_char_number and entire_number:
+                            numbers_converted.append(int(entire_number))
+                            if not surroundingsDigits(content, i, j, entire_number):
+                                numbers_subtracted.append(int(entire_number))
+                            entire_number = ''
+                        previous_char_number = False
+
+            total = [int(x) for x in numbers_converted]
+            total_subtraction = [int(x) for x in numbers_subtracted]
+
+            print(f"Result: {sum(total) - sum(total_subtraction)}")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     except FileNotFoundError:
         print("The file does not exist.")
 
 
 if __name__ == '__main__':
-    #first_challenge('firstDecember.txt')
-    second_challenge('secondDecember.txt')
+    # first_challenge('firstDecember.txt')
+    # second_challenge('secondDecember.txt')
+    third_challenge('thirdDecember.txt')
